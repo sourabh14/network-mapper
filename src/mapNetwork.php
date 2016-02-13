5f4c7c03fb3d925c35d7f638e5b2471a3bda7837
Sourabh <simplysourabh_123@yahoo.com>
2016-02-13 15:25:09 +0530

    Added link coloring based on state value

--- src/mapNetwork.php
+++ src/mapNetwork.php
@@ -1,14 +1,27 @@
 <!DOCTYPE html>
 <html>
+	<head>
+        <meta http-equiv="content-type" content="text/html;charset=utf-8">
+        <title>Network Mapper</title>
+        <script type="text/javascript" src="d3.v2.js"></script>
+    </head>
 	<body>
 		Execute mapper
 		<?php
 			// execute mapper
 			$ret = exec('./mapper');
-			echo "return value : ". $ret;
-			
-			// Redirect to display graph
-			header("Location: displayGraph.html");
+			//echo "return value : ". $ret;
 		?>
+		
+		<svg id="cloud" width="1000" height="1000">
+            <defs>
+                <marker id="arrow" viewbox="0 -5 10 10" refX="18" refY="0"
+                        markerWidth="6" markerHeight="6" orient="auto">
+                    <path d="M0,-5L10,0L0,5Z">
+                </marker>
+           </defs>
+        </svg>
+        <link href="net.css" rel="stylesheet" type="text/css" />
+        <script src="net.js" type="text/javascript"></script>
 	</body>
 </html>	
--- src/net.js
+++ src/net.js
@@ -4,8 +4,8 @@ var height = 800;
 var color = d3.scale.category10();
 
 var force = d3.layout.force()
-    .charge(-180)
-    .linkDistance(70)
+    .charge(-80)
+    .linkDistance(120)
     .size([width, height]);
 
 var svg = d3.select("#cloud");
@@ -19,24 +19,40 @@ d3.json("./ping-module/linkState.json", function(json) {
     var links = svg.append("g").selectAll("line.link")
         .data(force.links())
         .enter().append("line")
-        .attr("class", "link");
+        .attr("class", "link")
+		.attr('stroke-width', 1000)
+        .style("stroke", function(d){
+				if(d.value == 0){
+					return "#f90000";
+				}
+				else if(d.value == 1){
+					return "#00f900";
+				}
+				else{
+					return "grey";
+				}
+			}
+		);
 
     var nodes = svg.selectAll("circle.node")
         .data(force.nodes())
         .enter().append("circle")
         .attr("class", "node")
-        .attr("r", 8)
-        .style("fill", function(d) { return color(d.group); })
+        .attr("r", 10)
+        .style("fill", function(d) { return "skyblue"; })
         .call(force.drag);
 
-    nodes.append("title")
-        .text(function(d) { return d.name; });
+	    nodes.append("text")
+			   	 .attr("dy", ".35em")
+		.text(function(e) { return e.id; });
 
     force.on("tick", function() {
+
         links.attr("x1", function(d) { return d.source.x; })
             .attr("y1", function(d) { return d.source.y; })
             .attr("x2", function(d) { return d.target.x; })
             .attr("y2", function(d) { return d.target.y; });
+	     
 
         nodes.attr("cx", function(d) { return d.x; })
             .attr("cy", function(d) { return d.y; });
